61d3bcad77d341ac49a336dc6af63c9cdfa59cb5
rjohnson0707 <rwilljohnson@icloud.com>
2020-03-20 15:16:50 -0600

    SignIn/Signup/Logout functionality

---
+++ mysite/dwmf/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid Signup - Please try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
+
+def trucks_index(request):
+    return render(request, 'trucks/index.html')
+
+
+
+
