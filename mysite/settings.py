3defe88a708a464fcd89d5341b10455e8a48b760
ryanwfin <ryan.finch4416@gmail.com>
2020-03-25 18:26:57 -0600

    up and running again

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,13 +74,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
+DATABASES= {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dwmf',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
